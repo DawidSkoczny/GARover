f4aeef213a1ed26c2f80b81b8c35f0df361fb94e
mcksyrek <mcksyrek@gmail.com>
2017-12-11 22:17:53 +0100

    Add files via upload

---
+++ main.m
@@ -0,0 +1,10 @@
+% kiedy odrzuca zbyt du¿¹ liczbe rozwi¹zañ to mo¿e siê wysypaæ
+mapa=rand(10);
+populacja=start(100,5);
+kontrol=kontrola(mapa, populacja, 6, 8); %
+populacja1=mutowanie(populacja,1);
+kontrol1=kontrola(mapa, populacja1, 6, 8)
+populacja2=mutowanie(kontrol1,1);
+kontrol2=kontrola(mapa, populacja2, 6, 8)
+populacja3=mutowanie(kontrol2,2);
+kontrol3=kontrola(mapa, populacja3, 6, 8)
---
+++ mutowanie.m
@@ -0,0 +1,14 @@
+% funkcja mieszaj¹ca losowo cechy populacji
+function [ populacja ] = mutowanie( populacja_startowa, ile_cech_mieszac)
+populacja= populacja_startowa;
+[m,n] = size(populacja);
+for i = 1:m; 
+    x=0;
+    while x<ile_cech_mieszac
+      a=randi([1 n]);
+      populacja(i,a)=populacja_startowa(randi([1 m]),a);
+      x=x+1;
+    end
+end
+end
+
